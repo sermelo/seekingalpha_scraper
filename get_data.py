7790cca8e096a6ec31824ef49c8815c57a7600f4
Sergio Merino Alonso <sergio.merino.a@gmail.com>
2020-11-01 11:15:26 +0000

    Raise exception if it is not 403

--- get_data.py
+++ get_data.py
@@ -38,6 +38,8 @@ def request_data(symbol, next_id=None):
             response = do_request(url)
             done = True
         except requests.HTTPError as err:
+            if err.response.status_code != 403:
+                raise
             print(f'HTTP error for symbol {symbol} and next_id {next_id}: {err}')
             print(url)
             print('Retrying')
